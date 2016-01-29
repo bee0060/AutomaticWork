81d213c77ed16ff0065383e5821896b489229fba
Steven Weng <Steven_Weng@epam.com>
2016-01-29 09:23:42 +0800

    新项目，修改bat脚本

--- EPAM_AUTO_RUN_ALL_SOFTWARE.bat
+++ EPAM_AUTO_RUN_ALL_SOFTWARE.bat
@@ -2,15 +2,12 @@
 :: AUTO OPEN ALL SOFTWARE I NEED AT WORK, ONLY FOR EPAM.::
 :: AUTHOR: STEVEN WENG 									::
 :: CREATED ON: 2015/10/09								::
+:: MODIFIED ON: 2016/01/29								::
 :: =====================================================::
 
 
-:: WAIT 5 SECONDS FOR OS STARTUP
-TIMEOUT /t 5
-
-:: DIRECT TO WORK FOLDER
-cd D:\
-cd "src\hkgta\sino-hkgta-fe"
+:: WAIT 2 SECONDS FOR OS STARTUP
+TIMEOUT /t 2
  
 
 :: OPEN GIT BASH
@@ -20,12 +17,10 @@ START "" "D:\Develop Support\Git\Bin\sh.exe" --login -i
 START "" "D:\Develop Tools\Sublime Text 2\sublime_text.exe"
 
 :: OPEN THE WEB PAGES ARE NEED
-START /max "" "https://jirapct.epam.com/jira/secure/RapidBoard.jspa?rapidView=35866&view=planning.nodetail"
+START /max "" "https://jirapct.epam.com/"
 START /max "" "https://translate.google.com/?hl=zh-CN&tab=TT"
 START /max "" "https://pmcpct.epam.com/pmc/login.do"
 
-:: OPEN THE DEV PAGE ENV
-START /max "" "http://localhost/#/"  --new-window
 
 :: OPEN THE PERSONAL SOFTWARE
 START "" "D:\Program Files (x86)\Tencent\QQ\Bin\QQScLauncher.exe"
@@ -37,9 +32,6 @@ START "" "C:\Program Files (x86)\Skype\Phone\Skype.exe"
 :: OPEN OUTLOOK ON TASK BAR
 START "" "C:\Users\Steven_Weng\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Outlook 2013.lnk"
 
-
-:: RUN HTTP-SERVER AND WATCH
-bash fe live
 
 :: LYNC WILL AUTO RUN WHEN LOGIN WINDOW
 
