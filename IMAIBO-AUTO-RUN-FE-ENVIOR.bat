584c35ac549af7e1e7bdeb3ffdcabcd7967f3a7a
Steven Weng <Steven_Weng@epam.com>
2016-02-02 18:49:48 +0800

    添加imaibo自动启动前端bat 归档备份GTA的自动化bat

---
+++ IMAIBO-AUTO-RUN-FE-ENVIOR.bat
@@ -0,0 +1,127 @@
+:: =====================================================::
+:: AUTO OPEN IMAIBO FE ENVIORMENT.						::
+:: AUTHOR: STEVEN WENG 									::
+:: CREATED ON: 2016/02/02								::
+:: =====================================================::
+
+:: mention
+echo off
+echo ---------------------------------------------------------------------
+echo.
+echo                             iMaibo  FE                               
+echo.
+echo ---------------------------------------------------------------------
+echo.
+echo Please be sure imaibo-fe, imaibo-stockmarket, imaibo-portfolio project and redis DB（\Redis-x64-2.8.2101 folder）are in the same root folder, e.g.:
+echo \imaibo
+echo   \imaibo-fe
+echo   \imaibo-stockmarket
+echo   \imaibo-portfolio
+echo   \Redis-x64-2.8.2101
+echo  ---------------------------------------------------------------------
+echo.
+
+
+set /p letter=Please enter the project's letter(e.g. D), blank will use current letter: 
+set /p rootPath=Please enter the root folder path of prjects(e.g. src\imaibo), blank will use current folder:
+
+echo Please enter the backend profiles you want(e.g. local2qadb), 
+set /p profiles=.    blank will use the first profiles in project's application.yml:
+echo.
+echo.
+
+if defined letter (
+	cd %letter%:\
+
+)
+
+
+if defined rootPath (
+	cd "%rootPath%"
+)
+
+
+If NOT EXIST "Redis-x64-2.8.2101" (
+	echo Could not find redis folder, batch will fail and exit.
+	PAUSE
+	EXIT
+)
+
+If NOT EXIST "imaibo-fe" (
+	echo Could not find imaibo-fe folder, batch will fail and exit.
+	PAUSE
+	EXIT
+)
+
+If NOT EXIST "imaibo-stockmarket" (
+	echo Could not find imaibo-stockmarket folder, batch will fail and exit.
+	PAUSE
+	EXIT
+)
+
+If NOT EXIST "imaibo-portfolio" (
+	echo Could not find imaibo-portfolio folder, batch will fail and exit.
+	PAUSE
+	EXIT
+)
+
+echo Simple checking is successful, press any key to run fe env....
+PAUSE
+
+echo on
+
+
+:: RUN redis
+cd Redis-x64-2.8.2101
+START cmd /C redis-server.exe redis.windows.conf
+cd ..
+
+
+:: RUN imaibo-stockmarket
+cd imaibo-stockmarket
+START cmd /C node app.js local
+cd ..
+
+:: redirect to imaibo-portfolio
+cd imaibo-portfolio
+
+
+:: RUN mock-auth-server
+cd mock-auth-server
+
+if defined profiles (
+	START cmd /C gradlew bootRun -P SPRING.PROFILES.ACTIVE=%profiles%
+) else (
+	START cmd /C gradlew bootRun
+)
+
+cd ..
+
+:: RUN portal
+cd portal
+
+if defined profiles (
+	START cmd /C gradlew bootRun -P SPRING.PROFILES.ACTIVE=%profiles%
+) else (
+	START cmd /C gradlew bootRun	
+)
+
+cd ..
+
+:: back to root folder
+cd ..
+
+:: run imaibo-fe
+cd imaibo-fe
+
+:: RUN express
+START cmd /C node server.js local
+
+:: RUN gulp watch for fe
+START bash gulp watch
+
+cd ..
+
+
+:: PAUSE COMMAND FOR DEBUG
+:: PAUSE
