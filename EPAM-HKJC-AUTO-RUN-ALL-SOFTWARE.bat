50ac08c5890c4de12b445747880e0879233d04f3
Steven Weng <Steven_Weng@epam.com>
2016-03-14 09:27:41 +0800

    add HKJC auto run batch file

---
+++ EPAM-HKJC-AUTO-RUN-ALL-SOFTWARE.bat
@@ -0,0 +1,33 @@
+:: =====================================================::
+:: AUTO OPEN ALL SOFTWARES NEEDDE, FOR HKJC project.	::
+:: AUTHOR: STEVEN WENG 									::
+:: CREATED ON: 201\6/03/14								::
+:: =====================================================::
+
+
+:: WAIT 3 SECONDS FOR OS STARTUP
+TIMEOUT /t 3
+
+:: OPEN SUBLIME TEXT 2 IF NOT OPENED YET
+START "" "D:\Develop Tools\Sublime Text 2\sublime_text.exe"
+
+:: OPEN THE WEB PAGES ARE NEED
+START /max "" "https://jirapct.epam.com/jira/secure/RapidBoard.jspa?rapidView=39268&view=planning.nodetail"
+START /max "" "https://translate.google.com/?hl=zh-CN&tab=TT"
+START /max "" "https://pmcmsq.epam.com/pmc/login.do?tz=GMT%2B08:00"
+
+:: OPEN THE PERSONAL SOFTWARE
+START "" "D:\Program Files (x86)\Tencent\QQ\Bin\QQScLauncher.exe"
+
+
+:: OPEN SKYPE
+START "" "C:\Program Files (x86)\Skype\Phone\Skype.exe"
+
+:: OPEN OUTLOOK ON TASK BAR
+START "" "C:\Users\Steven_Weng\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Outlook 2013.lnk"
+
+
+:: LYNC WILL AUTO RUN WHEN LOGIN WINDOW
+
+:: PAUSE COMMAND FOR DEBUG
+:: PAUSE
