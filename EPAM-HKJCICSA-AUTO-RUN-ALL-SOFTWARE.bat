85d96069696fb48813c8a7fff4981c4a6db7c441
Steven_Weng <Steven_Weng@epam.com>
2017-02-22 10:49:47 +0800

    update HKJC-ICSA auto batch

--- EPAM-HKJCICSA-AUTO-RUN-ALL-SOFTWARE.bat
+++ EPAM-HKJCICSA-AUTO-RUN-ALL-SOFTWARE.bat
@@ -9,13 +9,14 @@
 TIMEOUT /t 1
 
 :: OPEN SUBLIME TEXT 2 IF NOT OPENED YET
- START "" "D:\Program Files\Sublime Text 2\sublime_text.exe"
+ ::START "" "D:\Program Files\Sublime Text 2\sublime_text.exe"
 
 :: OPEN THE WEB PAGES ARE NEED
 START /max "" "https://jirapct.epam.com/jira/secure/RapidBoard.jspa?rapidView=40750"
 START /max "" "https://jirapct.epam.com/jira/secure/RapidBoard.jspa?rapidView=40724"
 START /max "" "https://translate.google.com/?hl=zh-CN&tab=TT"
 START /max "" "https://pmc.epam.com/pmc/timejournal/list.do?projectId=4060741400037942733"
+START /max "" "https://kb.epam.com/display/HKJCICSA/HKJC+EPAM+DEV+Environment"
 
 :: OPEN THE PERSONAL SOFTWARE
 START "" "D:\Program Files (x86)\Tencent\QQ\Bin\QQScLauncher.exe"
@@ -32,14 +33,14 @@ START "" "C:\Users\Steven_Weng\AppData\Roaming\Microsoft\Internet Explorer\Quick
 
 
 :: DIRECT TO WORK FOLDER
-cd D:\
-cd "src\HKJC-ICSA"
+::cd D:\
+::cd "src\HKJC-ICSA"
 
 :: OPEN GIT BASH
-START "" "D:\Program Files\Git\bin\sh.exe" --login -i
+::START "" "D:\Program Files\Git\bin\sh.exe" --login -i
 
 :: RUN HTTP-SERVER AND WATCH
-bash npm run dev
+::bash npm run dev
 
 
 :: PAUSE COMMAND FOR DEBUG
