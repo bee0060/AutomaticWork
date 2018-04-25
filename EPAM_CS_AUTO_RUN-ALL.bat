826bdaeea74055ed9a2337e01e735204f0681ea0
Steven Weng <Steven_Weng@epam.com>
2018-04-25 10:51:26 +0800

    change cs auto batch

--- EPAM_CS_AUTO_RUN-ALL.bat
+++ EPAM_CS_AUTO_RUN-ALL.bat
@@ -13,3 +13,11 @@ TIMEOUT /t 1
 START /max "" "https://translate.google.com.hk/?hl=zh-CN"
 START /max "" "https://github.com/"
 START /max "" "https://partner-sg.credit-suisse.com/vpn/partner.html"
+
+:: OPEN THE PERSONAL SOFTWARE
+START "" "C:\Program Files (x86)\Tencent\QQ\Bin\QQ.exe"
+
+:: OPEN OUTLOOK ON TASK BAR
+START "" "C:\Users\Steven_Weng\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Outlook"
+
+:: LYNC WILL AUTO RUN WHEN LOGIN WINDOW
