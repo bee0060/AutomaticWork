4282088d65bcd013c70796d1e8c550a56a4520c6
Steven_Weng <Steven_Weng@epam.com>
2019-10-14 10:35:57 +0800

    add bench auto file

---
+++ EPAM_BENCH.bat
@@ -0,0 +1,20 @@
+:: =====================================================::
+:: AUTO OPEN ALL SOFTWARES NEEDDE, FOR BENCH.	::
+:: AUTHOR: STEVEN WENG 									::
+:: CREATED ON: 2019/10/14								::
+:: =====================================================::
+
+
+:: WAIT 1 SECOND FOR OS STARTUP
+TIMEOUT /t 1
+
+
+:: OPEN THE WEB PAGES ARE NEED
+START /max "" "https://translate.google.com.hk/?hl=zh-CN"
+START /max "" "https://github.com/"
+
+:: OPEN THE PERSONAL SOFTWARE
+START "" "C:\Program Files (x86)\Tencent\QQ\Bin\QQ.exe"
+
+:: OPEN OUTLOOK ON TASK BAR
+START "" "C:\Users\Steven_Weng\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Outlook"
